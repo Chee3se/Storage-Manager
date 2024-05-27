119ed1a54c9ce936e2091859570e9b45219d5ffa
KandzaP <kandzapure@gmail.com>
2024-05-27 14:16:02 +0300

    changed views

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -19,7 +19,7 @@ export default function Authenticated({ user, header, children }) {
                         <div className="flex w-full justify-center">
 
 
-                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex ">
+                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex gap-5">
                                 <NavLink href={route('home')} active={route().current('home')}>
                                     Home
                                 </NavLink>
@@ -41,11 +41,12 @@ export default function Authenticated({ user, header, children }) {
                                                 <span className="inline-flex rounded-md">
                                                     <button
                                                         type="button"
-                                                        className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
+                                                        className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150 "
                                                     >
                                                         {user.name}
 
-                                                        <svg
+                                                        <svg 
+                                                            
                                                             className="ms-2 -me-0.5 h-4 w-4"
                                                             xmlns="http://www.w3.org/2000/svg"
                                                             viewBox="0 0 20 20"
