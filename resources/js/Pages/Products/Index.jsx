301d1decdd73cedb00b00510327fbc2b73392615
neviens <tavamamma@example.com>
2024-06-03 11:03:52 +0300

    stye

--- resources/js/Pages/Products/Index.jsx
+++ resources/js/Pages/Products/Index.jsx
@@ -4,7 +4,7 @@ import { Head } from '@inertiajs/react';
 import AddProductForm from '@/Components/Products/AddProductForm';
 
 export default function Index({ auth, products }) {
-    const [showProductForm, setShowProductForm] = useState(false); 
+    const [showProductForm, setShowProductForm] = useState(false);
 
     const toggleProductForm = () => {
         setShowProductForm(!showProductForm);
@@ -19,13 +19,13 @@ export default function Index({ auth, products }) {
                         onClick={toggleProductForm}
                         className="bg-green-700 p-2 rounded-lg text-white hover:bg-green-600 duration-200 w-10 h-10 m-2"
                     >
-                       
+
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className='w-10 h-10'>
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                         </svg>
                     </button>
                 </div>
-                <div className="max-w-7xl sm:px-6 lg:px-8 grid lg:grid-cols-3 sm:grid-cols-2 items-center">
+                <div className="max-w-7xl sm:px-6 lg:px-8 grid lg:grid-cols-3 sm:grid-cols-2 items-center mx-auto">
                     {products.map(product => (
                         <div key={product.id} className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg w-80 border-0 hover:border-2 hover:border-black dark:hover:border-2 dark:hover:border-white mb-10">
                             <div className="flex-col flex text-gray-900 dark:text-gray-100 justify-center">
